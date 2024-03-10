4c7c1162a1bd5f1a83216a3f2897b5c145962f0e
maryammbabajanizan-eng <162549571+maryammbabajanizan-eng@users.noreply.github.com>
2024-03-10 04:10:30 -0700

    Add files via upload

---
+++ #برنامه ای بنویسید که 3 عدد دریافت کنه و.py
@@ -0,0 +1,6 @@
+#برنامه ای بنویسید که 3 عدد دریافت کنه و میانگین ان ها را بگیره
+num1=int(input('pleas enter number'))
+num2=int(input('pleas enter number'))
+num3=int(input('pleas enter number'))
+sum=(num1+num2+num3==num),num/3
+print(x)
